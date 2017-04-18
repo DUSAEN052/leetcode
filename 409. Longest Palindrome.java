fc662643d2e4b48d2262d79a8965370b6de38693
dusaen052 <norb662@gmail.com>
2017-04-18 03:02:25 -0400

    set3

---
+++ 409. Longest Palindrome.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int longestPalindrome(String s) {
+        int[] arr = new int[58];
+        boolean odd = false;
+        boolean single = false;
+        int len = 0;
+        for ( int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'A'] +=1;
+        }
+        for (int j = 0; j < 58; j++) {
+            if (arr[j]%2 == 0) {
+                len = len + arr[j];
+            } else {
+                odd = true;
+                len = len + arr[j] - 1;
+            }
+        }
+        
+        if (odd) {
+            return len + 1;
+        }
+        
+        return len;
+    }
+}
