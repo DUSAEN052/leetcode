64f590d9402475e60c15326dbb3b904250b3d214
Norbertlm <noobertlm@gmail.com>
2018-03-29 23:38:31 -0400

    clean up

--- 763. Partition Labels.py
+++ 763. Partition Labels.py
@@ -1,26 +1,22 @@
+from collections import Counter
+
 class Solution:
     def partitionLabels(self, S):
         """
         :type S: str
         :rtype: List[int]
         """
-        letters = {}
+        letters = Counter(S)
         checked = set()
         output = []
         count = 0
-        
-        for letter in S:
-            if letter in letters:
-                letters[letter] = letters[letter] + 1
-            else:
-                letters[letter] = 1
         
         for letter in S:
             checked.add(letter)
             
             if letters[letter] > 0 and checked:
                 count += 1
-                letters[letter] = letters[letter] - 1
+                letters[letter] -= 1
                 
                 if letters[letter] == 0:
                     checked.remove(letter)
