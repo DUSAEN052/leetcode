971387606566094b82b95b9096c337757070c233
Norbertlm <noobertlm@gmail.com>
2018-04-12 22:56:48 -0400

    new solutions

--- 494. Target Sum.py
+++ 494. Target Sum.py
@@ -6,11 +6,9 @@ class Solution:
         :rtype: int
         """
         def arrange(nums, current, hm, count, target):
-            if current == len(nums) and count == target:
-                return 1
-            elif current == len(nums) and count != target:
-                return 0
-            if (current, count) in hm:
+            if current == len(nums):
+                return 1 if count == target else 0
+            elif (current, count) in hm:
                 return hm[(current, count)]
             
             plus = arrange(nums, current + 1, hm, count + nums[current], target)
@@ -19,8 +17,5 @@ class Solution:
             hm[(current, count)] = total
             
             return total
-
-        plus = 0
-        plus += arrange(nums, 0, {}, 0, S)
         
-        return plus
+        return arrange(nums, 0, {}, 0, S)
