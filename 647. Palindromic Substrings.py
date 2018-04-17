0a404781b2bbd25d7b45e2c047577d7d175b1405
Norbertlm <noobertlm@gmail.com>
2018-04-16 21:30:58 -0400

    647 solution

---
+++ 647. Palindromic Substrings.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        def build(s, left, right):
+            total = 0
+            
+            while left >= 0 and right < len(s):
+                if s[left] == s[right]:
+                    total += 1
+                    left -= 1
+                    right += 1
+                else:
+                    break
+            
+            return total
+        
+        total = 0
+        
+        for i in range(len(s)):
+            total += build(s, i, i)
+            total += build(s, i, i + 1)
+        
+        return total
