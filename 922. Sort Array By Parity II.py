7caf6847337fe9c47b16849f002759b134ca00c4
Norbertlm <noobertlm@gmail.com>
2018-11-15 20:03:40 -0500

    new solution

---
+++ 922. Sort Array By Parity II.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortArrayByParityII(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        odd = [a for a in A if a % 2 != 0]
+        even = [a for a in A if a % 2 == 0]
+        return [even.pop() if i % 2 == 0 else odd.pop() for i in range(len(A)) ]
