0d345a6f40eef31393d009b84a3b3e45accfe927
Norbertlm <noobertlm@gmail.com>
2018-11-15 21:34:02 -0500

    fixed spacing 922

--- 922. Sort Array By Parity II.py
+++ 922. Sort Array By Parity II.py
@@ -6,4 +6,4 @@ class Solution:
         """
         odd = [a for a in A if a % 2 != 0]
         even = [a for a in A if a % 2 == 0]
-        return [even.pop() if i % 2 == 0 else odd.pop() for i in range(len(A)) ]
+        return [even.pop() if i % 2 == 0 else odd.pop() for i in range(len(A))]
