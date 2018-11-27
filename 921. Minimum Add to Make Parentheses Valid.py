33f3913f28d381aca4c90213057e55687fd5a47d
Norbertlm <noobertlm@gmail.com>
2018-11-26 20:20:12 -0500

    update 921

--- 921. Minimum Add to Make Parentheses Valid.py
+++ 921. Minimum Add to Make Parentheses Valid.py
@@ -4,16 +4,16 @@ class Solution:
         :type S: str
         :rtype: int
         """
-        stck = []
+        stck = 0
         count = 0
         
         for s in S:
             if s == '(':
-                stck.append(s)
+                stck += 1
             else:
                 if not stck:
                     count += 1
                 else:
-                    stck.pop()
+                    stck -= 1
         
-        return count + len(stck)     
+        return count + stck     
