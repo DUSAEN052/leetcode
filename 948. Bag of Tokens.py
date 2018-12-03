3c6f2b4548017e95806a12023cb1884da772f6a4
Norbertlm <noobertlm@gmail.com>
2018-12-03 01:02:51 -0500

    solution 948

---
+++ 948. Bag of Tokens.py
@@ -0,0 +1,22 @@
+from collections import deque
+class Solution:
+    def bagOfTokensScore(self, tokens, P):
+        """
+        :type tokens: List[int]
+        :type P: int
+        :rtype: int
+        """
+        T = deque(sorted(tokens))
+        mx, points = 0, 0
+        
+        while T and (T[0] <= P or points):
+            if T[0] <= P:
+                P -= T.popleft()
+                points += 1
+            else:
+                if points >= 1 and T[-1] > P:
+                    P += T.pop()
+                    points -= 1
+            mx = max(mx, points)
+        
+        return mx
