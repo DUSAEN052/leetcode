8977f4e5d85181d9cc6d966a5b520e1ecfbcfbc3
Norbertlm <noobertlm@gmail.com>
2018-12-06 20:45:17 -0500

    refactor 216

--- 216. Combination Sum III.py
+++ 216. Combination Sum III.py
@@ -9,14 +9,15 @@ class Solution:
         
         def find(k, n, arr, total, curr):
             if k == 0 and total == n:
-                output.append(arr)
+                output.append(arr[:])
                 return
             elif k == 0:
                 return
     
             for i in range(curr + 1, 10):
-                newArr = arr[:] + [i]
-                find(k - 1, n, newArr, total + i, i)
+                arr.append(i)
+                find(k - 1, n, arr, total + i, i)
+                arr.pop()
         
         for i in range(1, 10):
             find(k - 1, n, [i], i, i)
