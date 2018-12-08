7db46c719e51d48374e2a98daa2c34e655d74c1b
Norbertlm <noobertlm@gmail.com>
2018-12-08 17:45:59 -0500

    refactor 216

--- 216. Combination Sum III.py
+++ 216. Combination Sum III.py
@@ -5,9 +5,7 @@ class Solution:
         :type n: int
         :rtype: List[List[int]]
         """
-        output = []
-        
-        def find(k, n, arr, total, curr):
+        def find(k, n, arr, total, curr, output):
             if k == 0 and total == n:
                 output.append(arr[:])
                 return
@@ -16,10 +14,12 @@ class Solution:
     
             for i in range(curr + 1, 10):
                 arr.append(i)
-                find(k - 1, n, arr, total + i, i)
+                find(k - 1, n, arr, total + i, i, output)
                 arr.pop()
         
+        output = []
+        
         for i in range(1, 10):
-            find(k - 1, n, [i], i, i)
+            find(k - 1, n, [i], i, i, output)
         
         return output
