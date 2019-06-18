7d73300570c818cd4319e9cd8c46a28a1af617ee
Norbertlm <noobertlm@gmail.com>
2019-06-18 18:49:19 -0400

    solution 1089

---
+++ 1089. Duplicate Zeros.py
@@ -0,0 +1,14 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        length = len(arr)
+        index = 0
+        
+        while index < length:
+            if arr[index] == 0:
+                arr.insert(index, 0)
+                index += 1
+                arr.pop()
+            index += 1
